00be5be063a78dd57d92bd97e5727af4038e4a3a
MarcoBaygorria <baygorria91@gmail.com>
2026-01-27 12:57:28 -0300

    Manejo de errores e injectar pokemon

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,14 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PokemonService {
 
+  //Crear pokemon en base de datos.1
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) { }
 
-  create(createPokemonDto: CreatePokemonDto) {
+
+  async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase()
-    return createPokemonDto;
+
+    try {
+      //Crear pokemon en base de datos.2
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+
+    } catch (error) {
+      //Manejo de errores
+      if (error.code === 11000) {
+        throw new BadRequestException(`Pokemon existe en la db ${JSON.stringify(error.keyValue)}`)
+      }
+      throw new InternalServerErrorException(`No se pudo crear el pokemon - Revisa los logs del servidor`)
+    }
   }
 
   findAll() {
